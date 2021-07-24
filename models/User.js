caba61564b2eafa157423310dd2cb4297f48c5d5
Sebastian <sebastianalvarado246801@gmail.com>
2021-07-24 13:32:24 -0500

    user model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, Types } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/, 'Must be a valid email address!']
+        },
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+            }
+        ]
+        },
+          {
+            toJSON: {
+              virtuals: true,
+            },
+            id: false
+    }
+)
+
+userSchema.virtual('friendCount').get(function () {
+    if (!this.friends) {
+        return 0
+    }
+    return this.friends.length
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
